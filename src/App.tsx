a1765f882cd0be367d1fc3c3250e45507748c6ab
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-03-13 18:10:34 +0530

    feat: setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom'
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <header>Header</header>
+    <Routes>
+      <Route path="/" element={<main>Home</main>} />
+      <Route path='*' element={<div>Not found</div>} />
+    </Routes>
+    <footer>Footer</footer>
+  </>;
 }
 
 export default App;
