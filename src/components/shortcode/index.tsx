adfabaeca8bf7317f9cab2c21df57f8e9f5e18de
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-04-06 10:21:02 +0530

    feat: add qr based urls

--- src/components/shortcode/index.tsx
+++ src/components/shortcode/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Group, Text, Tooltip } from "@mantine/core";
+import { Button, Group, Text, Tooltip, useMantineTheme } from "@mantine/core";
 import { useClipboard } from '@mantine/hooks'
 import { Link } from "react-router-dom";
+import { QRCodeSVG } from 'qrcode.react';
 
 type Props = {
   base_uri : string
@@ -11,14 +12,20 @@ type Props = {
 
 export default function Shortcode({ base_uri, shortcode, external=false, querystring={} }: Props) {
   const clipboard = useClipboard({timeout: 3000})
+  const theme = useMantineTheme()
   let full_url = `${base_uri.replace(/\/+$/, '')}/${shortcode}`
   if (Object.keys(querystring).length > 0) {
     full_url += '?' + Object.entries(querystring).map(([key, value]) => `${key}=${value}`).join('&')
   }
   const anchorCommonProps = { children: shortcode, style: {padding: 3} }
 
-  return <Group align='center'>
-    <Group direction='column' align='center' spacing={0}>
+  let colors = {
+    fgcolor: theme.colorScheme === 'light' ? theme.colors.dark[9] : theme.colors.gray[0],
+    bgcolor: theme.colorScheme === 'light' ? theme.colors.gray[0] : theme.colors.dark[9],
+  }
+
+  return <Group align='center' style={{boxShadow: theme.shadows.xl, padding: theme.spacing.md}}>
+    <Group direction='column' align='center' spacing='sm'>
       <Text>Shortcode</Text>
       <Text style={{display: 'flex', alignItems: 'center', gap: 5}}>
         { external
@@ -32,5 +39,9 @@ export default function Shortcode({ base_uri, shortcode, external=false, queryst
         </Tooltip>
       </Text>
     </Group>
+    <Tooltip label='Scan this QR to open resource' withArrow>
+      <QRCodeSVG value={full_url} size={150} {...colors}
+        style={{ boxShadow: theme.shadows.md, padding: theme.spacing.xs }} />
+    </Tooltip>
   </Group>
 }
