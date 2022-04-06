d8b4bca6c2dd5af2494b17fe3cb86a63f9703294
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-04-06 10:48:28 +0530

    fix: absolute urls while copying

--- src/components/shortcode/index.tsx
+++ src/components/shortcode/index.tsx
@@ -14,9 +14,11 @@ export default function Shortcode({ base_uri, shortcode, external=false, queryst
   const clipboard = useClipboard({timeout: 3000})
   const theme = useMantineTheme()
   let full_url = `${base_uri.replace(/\/+$/, '')}/${shortcode}`
+
   if (Object.keys(querystring).length > 0) {
     full_url += '?' + Object.entries(querystring).map(([key, value]) => `${key}=${value}`).join('&')
   }
+  const copy_url = full_url.includes('http') ? full_url : `${window.location.origin}${full_url}`
   const anchorCommonProps = { children: shortcode, style: {padding: 3} }
 
   let colors = {
@@ -29,18 +31,18 @@ export default function Shortcode({ base_uri, shortcode, external=false, queryst
       <Text>Shortcode</Text>
       <Text style={{display: 'flex', alignItems: 'center', gap: 5}}>
         { external
-          ? <Button component='a' target='_blank' href={full_url} variant='subtle' {...anchorCommonProps} />
+          ? <Button component='a' target='_blank' href={copy_url} variant='subtle' {...anchorCommonProps} />
           : <Button component={Link} to={full_url} variant='subtle' {...anchorCommonProps} />
         }
         <Tooltip label='Copied' opened={clipboard.copied}>
-          <Button variant='outline' size='sm' onClick={() => clipboard.copy(full_url)} disabled={clipboard.copied}>
+          <Button variant='outline' size='sm' onClick={() => clipboard.copy(copy_url)} disabled={clipboard.copied}>
             {clipboard.copied ? 'Copied' : 'Copy'}
           </Button>
         </Tooltip>
       </Text>
     </Group>
     <Tooltip label='Scan this QR to open resource' withArrow>
-      <QRCodeSVG value={full_url} size={150} {...colors}
+      <QRCodeSVG value={copy_url} size={150} {...colors}
         style={{ boxShadow: theme.shadows.md, padding: theme.spacing.xs }} />
     </Tooltip>
   </Group>
